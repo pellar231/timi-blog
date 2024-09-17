96826bdbdcdda7098f17c0ccc4cf76b803c62466
Odufowokan ayotomiwa <102088546+Timmi-tech@users.noreply.github.com>
2024-09-17 22:43:57 +0100

    Initial commit

--- Blog/settings.py
+++ Blog/settings.py
@@ -22,12 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^bs=8sdmljpt*(+ll67o_!+v@2hb*-rxa6+ea5$2dnx-co942u'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+
 
 
 # Application definition
@@ -84,7 +86,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-DATABASES['default'] =dj_database_url.parse("postgresql://myecommerce_poor_user:9FlcrIeRSbB6djnz3w9GS9FEbGdpn3Lc@dpg-crkt92u8ii6s7386qqb0-a.oregon-postgres.render.com/myecommerce_poor")
+database_url = os.environ.get("DATABASE_URL")
+DATABASES['default'] = dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
